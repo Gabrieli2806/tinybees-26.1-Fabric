7f7ad03778b61ba56e4f45cddd14001e08d3f550
Gabrieli2806 <[tu-email@ejemplo.com]>
2026-03-25 16:09:07 -0500

    Final version

--- src/main/java/com/g2806/tinybees/mixin/BeeEntityMixin.java
+++ src/main/java/com/g2806/tinybees/mixin/BeeEntityMixin.java
@@ -8,8 +8,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Bee.class)
 public class BeeEntityMixin {
-	@Inject(at = @At("TAIL"), method = "<init>")
-	private void onInit(CallbackInfo info) {
-		((Bee) (Object) this).setBaby(true);
+	@Inject(at = @At("HEAD"), method = "tick")
+	private void onTick(CallbackInfo info) {
+		Bee bee = (Bee) (Object) this;
+		bee.getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.SCALE).setBaseValue(0.5D);
 	}
 }
